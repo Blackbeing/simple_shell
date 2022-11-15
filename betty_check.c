37ce427db424064c2d7e5ab195859cf7eb3877d7
Blackbeing <jonnieey55@gmail.com>
2022-11-15 15:11:52 +0000

    Init Test

---
+++ betty_check.c
@@ -0,0 +1,11 @@
+#include "shell.h"
+
+/**
+ *  betty_checker - Print 98
+ */
+
+void betty_checker(void)
+{
+	printf("%d\n", 98);
+}
+
---
+++ shell.h
@@ -0,0 +1,11 @@
+#ifndef _SHELL_
+#define _SHELL_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+#endif /* SHELL_H */
